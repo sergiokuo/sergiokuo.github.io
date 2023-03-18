28a51320e08c3a5a0b0ce4f277130868a0851741
SergioKuo <gsqikaros@outlook.com>
2023-03-18 22:37:48 +0800

    Site updated: 2023-03-18 22:37:44

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "1",
+      "language": "zh",
+      "borderRadius": "25",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "71896e28832a428c9c2058be1b1992de"
+    }
+  }
